74f0baa415a48a610f7458f349d7f835248d4092
sarahmocknick <sem319@georgetown.edu>
2023-12-04 18:52:48 -0500

    Updated Simple Dashboard

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -6,9 +6,11 @@ from web_app.routes.home_routes import home_routes
 from web_app.routes.simple_routes import simple_routes
 from web_app.routes.complex_routes import complex_routes
 
+SECRET_KEY = os.getenv("SECRET_KEY", default="abc123") 
 
 def create_app():
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = SECRET_KEY
     app.register_blueprint(home_routes)
     app.register_blueprint(simple_routes)
     app.register_blueprint(complex_routes)
@@ -18,3 +20,6 @@ def create_app():
 if __name__ == "__main__":
     my_app = create_app()
     my_app.run(debug=True)
+
+
+   
