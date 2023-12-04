0b4700196a4f68656871c7e6b9e60687ac3f13e3
sarahdoherty1 <142939713+sarahdoherty1@users.noreply.github.com>
2023-12-04 12:36:06 -0500

    trying again

--- web_app/routes/simple_routes.py
+++ web_app/routes/simple_routes.py
@@ -1,28 +1,18 @@
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, request, render_template, redirect, flash
 
-from app.simple import fetch_data  # Adjust the import path based on your file structure
+from app.simple import fetch_data
 
 simple_routes = Blueprint("simple_routes", __name__)
 
 
 @simple_routes.route("/simple/dashboard")
 def simple_dashboard():
-    print("SIMPLE JOB SEARCH DASHBOARD...")
-    # Fetch job data using the provided function
-    results = fetch_data()
-    if results:
-        return render_template("results.html", results=results)
-    else:
-        flash("Error fetching job data", "error")
-        return render_template("error.html")
+    print("Simple Job Search DASHBOARD...")
+
+
+
 
 
 @simple_routes.route("/api/simple.json")
 def simple_api():
-    print("SIMPLE JOB SEARCH DATA (API)...")
-    # Similar logic to fetch job data, can return JSON if needed
-    results = fetch_data()
-    if results:
-        return {"results": results}
-    else:
-        return {"error": "Error fetching job data"}
+    print("Simple Job Search DATA (API)...")
