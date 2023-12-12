d52562d375cac55138827d93cf99f8c35f3f370c
sarahdoherty1 <142939713+sarahdoherty1@users.noreply.github.com>
2023-12-11 19:32:26 -0500

    Updating descriptions (#9)
    * Updated README and Fixed errors
    
    * Job Search Command Line
    
    * Set Up Simple Form and Dashboard
    
    * Updated Simple Dashboard
    
    * update simple form
    
    * Adjusted Simple Job Search Output
    
    * Display Company Name and Location
    
    * Complex Job Search
    
    * Updating descriptions
    
    ---------
    
    Co-authored-by: sarahmocknick <sem319@georgetown.edu>
    Co-authored-by: sarahmocknick <142832943+sarahmocknick@users.noreply.github.com>

--- web_app/routes/complex_routes.py
+++ web_app/routes/complex_routes.py
@@ -1,20 +1,46 @@
-from flask import Blueprint, request, render_template, redirect, flash
+
+from flask import Blueprint, request, render_template, flash, jsonify, redirect
 
 from app.complex import complex_job_search
 
 complex_routes = Blueprint("complex_routes", __name__)
 
-
-@complex_routes.route("/complex/dashboard")
+@complex_routes.route("/complex/dashboard", methods=["GET", "POST"])
 def complex_dashboard():
-    print("Complex Job Search DASHBOARD...")
-    return "to do..."
+    if request.method == "POST":
+        what = request.form.get("what")
+        result = complex_job_search(what)
+        
+        if result is not None:
+            if isinstance(result, dict) and 'results' in result:
+                jobs = result['results']
+                if jobs:
+                    return render_template("complex_dashboard.html", jobs=jobs)
+                else:
+                    flash("No results found for the specified job query.", "warning")
+                    print(f"API returned empty results for query: {what}")  # Debugging
+            else:
+                flash("Error in retrieving job data. Please check your input and try again.", "danger")
+                print(f"Error retrieving data for query: {what}")  # Debugging
+                if isinstance(result, dict) and 'error' in result:
+                    print(f"API Error Message: {result['error']}") 
+        else:
+            flash("Error in retrieving job data. Please check your input and try again.", "danger")
+            print(f"Error retrieving data for query: {what}")  # Debugging
+        
+        return redirect("/complex/dashboard")  # Redirect to the form
 
-
-
-
+    return render_template("complex_form.html")  # Display the form initially
 
 @complex_routes.route("/api/complex.json")
-def simple_api():
-    print("Complex Job Search DATA (API)...")
-    return "to do..."
+def complex_api():
+    try:
+        data = complex_job_search()
+        if data is not None:
+            return jsonify(data)
+        else:
+            return {"message": "No data found."}, 404
+    except Exception as err:
+        print('OOPS', err)
+        return {"message": "Complex Job Search Data Error. Please try again."}, 404
+
